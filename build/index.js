a2b92015c74740e60d1a769fdffb0453e58d2b30
ndxbxrme <lewis_the_cat@hotmail.com>
2017-04-10 13:19:06 +0100

    monday

--- build/index.js
+++ build/index.js
@@ -12,7 +12,7 @@
   }
 
   module.factory('ndxCheck', function($rootScope, $window) {
-    var checkScope, doCheck;
+    var checkScope, doCheck, setPristine;
     checkScope = function(scope) {
       var key;
       for (key in scope) {
@@ -38,21 +38,51 @@
       }
       return true;
     };
+    setPristine = function(scope) {
+      var key;
+      for (key in scope) {
+        if (Object.prototype.toString.call(scope[key]) === '[object Object]') {
+          if (scope[key].$$controls) {
+            if (!scope[key].$$element[0].attributes.ignore) {
+              if (!scope[key].$pristine) {
+                scope[key].$setPristine();
+              }
+            }
+          }
+        }
+      }
+      if (scope.$$childHead) {
+        setPristine(scope.$$childHead);
+      }
+      if (scope.$$nextSibling) {
+        return setPristine(scope.$$nextSibling);
+      }
+    };
     doCheck = function(scope) {
       return checkScope(scope || $rootScope);
     };
     return {
       check: doCheck,
-      valid: doCheck
+      valid: doCheck,
+      setPristine: function(scope) {
+        return setPristine(scope || $rootScope);
+      }
     };
   });
 
-  module.run(function($window, ndxCheck) {
-    return $window.onbeforeunload = function(event) {
+  module.run(function($window, $rootScope, ndxCheck) {
+    $window.onbeforeunload = function(event) {
       if (!ndxCheck.check()) {
         return true;
       }
     };
+    return $rootScope.$on('$stateChangeStart', function(event) {
+      if (!ndxCheck.check()) {
+        if (!confirm('Changes you made may not be saved.  Are you sure you want to proceed?')) {
+          return event.preventDefault();
+        }
+      }
+    });
   });
 
 }).call(this);
--- src/index.coffee
+++ src/index.coffee
@@ -19,11 +19,28 @@ module.factory 'ndxCheck', ($rootScope, $window) ->
       if not checkScope scope.$$nextSibling
         return false
     return true
+  setPristine = (scope) ->
+    for key of scope
+      if Object.prototype.toString.call(scope[key]) is '[object Object]'
+        if scope[key].$$controls
+          if not scope[key].$$element[0].attributes.ignore
+            if not scope[key].$pristine
+              scope[key].$setPristine()
+    if scope.$$childHead
+      setPristine scope.$$childHead
+    if scope.$$nextSibling
+      setPristine scope.$$nextSibling
   doCheck = (scope) ->
     checkScope scope or $rootScope
   check: doCheck
   valid: doCheck
-module.run ($window, ndxCheck) ->
+  setPristine: (scope) ->
+    setPristine scope or $rootScope
+module.run ($window, $rootScope, ndxCheck) ->
   $window.onbeforeunload = (event) ->
     if not ndxCheck.check()
       return true
+  $rootScope.$on '$stateChangeStart', (event) ->
+    if not ndxCheck.check()
+      if not confirm 'Changes you made may not be saved.  Are you sure you want to proceed?'
+        event.preventDefault()
