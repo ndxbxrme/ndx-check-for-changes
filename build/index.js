3069efc18d266e8bf82566522844ad59e12be2cf
ndxbxrme <lewis_the_cat@hotmail.com>
2017-08-22 13:39:43 +0100

    $transitions and confirmFn

--- build/index.js
+++ build/index.js
@@ -8,80 +8,104 @@
     module = angular.module('ndx');
   } catch (error) {
     e = error;
-    module = angular.module('ndx-check-for-changes', []);
+    module = angular.module('ndx', []);
   }
 
-  module.factory('ndxCheck', function($rootScope, $window) {
-    var checkScope, doCheck, setPristine;
-    checkScope = function(scope) {
-      var key;
-      for (key in scope) {
-        if (Object.prototype.toString.call(scope[key]) === '[object Object]') {
-          if (scope[key].$$controls) {
-            if (!scope[key].$$element[0].attributes.ignore) {
-              if (!scope[key].$pristine) {
-                return false;
-              }
-            }
-          }
-        }
-      }
-      if (scope.$$childHead) {
-        if (!checkScope(scope.$$childHead)) {
-          return false;
-        }
-      }
-      if (scope.$$nextSibling) {
-        if (!checkScope(scope.$$nextSibling)) {
-          return false;
-        }
-      }
-      return true;
-    };
-    setPristine = function(scope) {
-      var key;
-      for (key in scope) {
-        if (Object.prototype.toString.call(scope[key]) === '[object Object]') {
-          if (scope[key].$$controls) {
-            if (!scope[key].$$element[0].attributes.ignore) {
-              if (!scope[key].$pristine) {
-                scope[key].$setPristine();
-              }
-            }
-          }
-        }
-      }
-      if (scope.$$childHead) {
-        setPristine(scope.$$childHead);
-      }
-      if (scope.$$nextSibling) {
-        return setPristine(scope.$$nextSibling);
-      }
-    };
-    doCheck = function(scope) {
-      return checkScope(scope || $rootScope);
+  module.provider('ndxCheck', function() {
+    var confirmFn;
+    confirmFn = function(cb) {
+      return cb(!confirm('Changes you made may not be saved.  Are you sure you want to proceed?'));
     };
     return {
-      check: doCheck,
-      valid: doCheck,
-      setPristine: function(scope) {
-        return setPristine(scope || $rootScope);
+      setConfirmFn: function(fn) {
+        return confirmFn = fn;
+      },
+      $get: function($rootScope, $window) {
+        var checkScope, doCheck, setPristine;
+        checkScope = function(scope) {
+          var key;
+          for (key in scope) {
+            if (Object.prototype.toString.call(scope[key]) === '[object Object]') {
+              if (scope[key].$$controls) {
+                if (!scope[key].$$element[0].attributes.ignore) {
+                  if (!scope[key].$pristine) {
+                    return false;
+                  }
+                }
+              }
+            }
+          }
+          if (scope.$$childHead) {
+            if (!checkScope(scope.$$childHead)) {
+              return false;
+            }
+          }
+          if (scope.$$nextSibling) {
+            if (!checkScope(scope.$$nextSibling)) {
+              return false;
+            }
+          }
+          return true;
+        };
+        setPristine = function(scope) {
+          var key;
+          for (key in scope) {
+            if (Object.prototype.toString.call(scope[key]) === '[object Object]') {
+              if (scope[key].$$controls) {
+                if (!scope[key].$$element[0].attributes.ignore) {
+                  if (!scope[key].$pristine) {
+                    scope[key].$setPristine();
+                  }
+                }
+              }
+            }
+          }
+          if (scope.$$childHead) {
+            setPristine(scope.$$childHead);
+          }
+          if (scope.$$nextSibling) {
+            return setPristine(scope.$$nextSibling);
+          }
+        };
+        doCheck = function(scope) {
+          return checkScope(scope || $rootScope);
+        };
+        return {
+          check: doCheck,
+          valid: doCheck,
+          setPristine: function(scope) {
+            return setPristine(scope || $rootScope);
+          },
+          setConfirmFn: function(fn) {
+            return confirmFn = fn;
+          },
+          confirmFn: confirmFn
+        };
       }
     };
   });
 
-  module.run(function($window, $rootScope, ndxCheck) {
+  module.run(function($window, $transitions, $q, ndxCheck) {
     $window.onbeforeunload = function(event) {
       if (!ndxCheck.check()) {
         return true;
       }
     };
-    return $rootScope.$on('$stateChangeStart', function(event) {
+    return $transitions.onBefore({}, function(trans) {
+      var defer;
+      defer = $q.defer();
       if (!ndxCheck.check()) {
-        if (!confirm('Changes you made may not be saved.  Are you sure you want to proceed?')) {
-          return event.preventDefault();
-        }
+        ndxCheck.confirmFn(function(res) {
+          if (res) {
+            return defer.reject('canceled by user');
+          } else {
+            return defer.resolve();
+          }
+        });
+      } else {
+        defer.resolve();
       }
+      return defer.promise;
     });
   });
 
--- src/index.coffee
+++ src/index.coffee
@@ -3,44 +3,59 @@ module = null
 try
   module = angular.module 'ndx'
 catch e
-  module = angular.module 'ndx-check-for-changes', []
-module.factory 'ndxCheck', ($rootScope, $window) ->
-  checkScope = (scope) ->
-    for key of scope
-      if Object.prototype.toString.call(scope[key]) is '[object Object]'
-        if scope[key].$$controls
-          if not scope[key].$$element[0].attributes.ignore
-            if not scope[key].$pristine
-              return false
-    if scope.$$childHead
-      if not checkScope scope.$$childHead
-        return false
-    if scope.$$nextSibling
-      if not checkScope scope.$$nextSibling
-        return false
-    return true
-  setPristine = (scope) ->
-    for key of scope
-      if Object.prototype.toString.call(scope[key]) is '[object Object]'
-        if scope[key].$$controls
-          if not scope[key].$$element[0].attributes.ignore
-            if not scope[key].$pristine
-              scope[key].$setPristine()
-    if scope.$$childHead
-      setPristine scope.$$childHead
-    if scope.$$nextSibling
-      setPristine scope.$$nextSibling
-  doCheck = (scope) ->
-    checkScope scope or $rootScope
-  check: doCheck
-  valid: doCheck
-  setPristine: (scope) ->
-    setPristine scope or $rootScope
-module.run ($window, $rootScope, ndxCheck) ->
+  module = angular.module 'ndx', []
+module.provider 'ndxCheck', ->
+  confirmFn = (cb) ->
+    cb not confirm 'Changes you made may not be saved.  Are you sure you want to proceed?'
+  setConfirmFn: (fn) ->
+    confirmFn = fn
+  $get: ($rootScope, $window) ->
+    checkScope = (scope) ->
+      for key of scope
+        if Object.prototype.toString.call(scope[key]) is '[object Object]'
+          if scope[key].$$controls
+            if not scope[key].$$element[0].attributes.ignore
+              if not scope[key].$pristine
+                return false
+      if scope.$$childHead
+        if not checkScope scope.$$childHead
+          return false
+      if scope.$$nextSibling
+        if not checkScope scope.$$nextSibling
+          return false
+      return true
+    setPristine = (scope) ->
+      for key of scope
+        if Object.prototype.toString.call(scope[key]) is '[object Object]'
+          if scope[key].$$controls
+            if not scope[key].$$element[0].attributes.ignore
+              if not scope[key].$pristine
+                scope[key].$setPristine()
+      if scope.$$childHead
+        setPristine scope.$$childHead
+      if scope.$$nextSibling
+        setPristine scope.$$nextSibling
+    doCheck = (scope) ->
+      checkScope scope or $rootScope
+    check: doCheck
+    valid: doCheck
+    setPristine: (scope) ->
+      setPristine scope or $rootScope
+    setConfirmFn: (fn) ->
+      confirmFn = fn
+    confirmFn: confirmFn
+module.run ($window, $transitions, $q, ndxCheck) ->
   $window.onbeforeunload = (event) ->
     if not ndxCheck.check()
       return true
-  $rootScope.$on '$stateChangeStart', (event) ->
+  $transitions.onBefore {}, (trans) ->
+    defer = $q.defer()
     if not ndxCheck.check()
-      if not confirm 'Changes you made may not be saved.  Are you sure you want to proceed?'
-        event.preventDefault()
+      ndxCheck.confirmFn (res) ->
+        if res
+          defer.reject 'canceled by user'
+        else
+          defer.resolve()
+    else
+      defer.resolve()
+    defer.promise
